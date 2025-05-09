6af89711898004bc0c48f8ab65d0587ab0e5780a
ajarodiy <ajarodiy@asu.edu>
2025-05-08 19:54:20 -0700

    trying to fix cors issue

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ajarodiy.me"],
+    allow_origins=["https://ajarodiy.me", "https://ajarodiy.me"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
